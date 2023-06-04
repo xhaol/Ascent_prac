f0c9e4ae768fa3d835377d75b196c18b1834f554
xhaol <135319189+xhaol@users.noreply.github.com>
2023-06-04 22:21:47 +0800

    Update ProductPanel.java

--- src/ui/ProductPanel.java
+++ src/ui/ProductPanel.java
@@ -1,20 +1,209 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.event.*;
+
+import bean.Product;
+import util.ProductDataClient;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+import java.io.*;
 
 /**
- * 这个类构建产品面板
- * 这是一个Java Swing GUI程序，用于展示商品信息的面板。
- * 该面板包括一个下拉列表框，用于选择商品类别，一个商品列表框，用于显示选定类别的商品，以及一些按钮，用于执行不同的操作，
- * 如显示商品详细信息、清空商品列表、退出程序等。
- * <p>
- * 该面板继承自JPanel类，具有自己的构造函数和方法。
- * 它还包括许多内部类，这些类实现了不同的事件监听器，用于处理按钮点击、下拉列表框选择等各种事件。
- * <p>
- * 该程序还涉及到网络连接和数据读取，使用了ProductDataClient类来获取商品和类别数据。
- *
- * @author cjc
+ * 商品面板，用于展示商品列表
+ * 
+ * @author ascent
  * @version 1.0
  */
+@SuppressWarnings("serial")
 public class ProductPanel extends JPanel {
+
+    protected JLabel selectionLabel;
+
+    protected JComboBox<String> categoryComboBox;
+
+    protected JPanel topPanel;
+
+    protected JList<Product> productListBox;
+
+    protected JScrollPane productScrollPane;
+
+    protected JButton detailsButton;
+
+    protected JButton clearButton;
+
+    protected JButton exitButton;
+
+    protected JButton shoppingButton;
+
+    protected JPanel bottomPanel;
+
+    protected MainFrame parentFrame;
+
+    protected ArrayList<Product> productArrayList;
+
+    protected ProductDataClient myDataClient;
+
+    /**
+     * 构造商品面板
+     * 
+     * @param theParentFrame 父窗口
+     */
+    public ProductPanel(MainFrame theParentFrame) {
+        try {
+            parentFrame = theParentFrame;
+            myDataClient = new ProductDataClient();
+            selectionLabel = new JLabel("选择类别");
+            categoryComboBox = new JComboBox<>();
+            categoryComboBox.addItem("-------");
+
+            ArrayList<String> categoryArrayList = myDataClient.getCategories();
+
+            for (String aCategory : categoryArrayList) {
+                categoryComboBox.addItem(aCategory);
+            }
+
+            topPanel = new JPanel();
+            productListBox = new JList<>();
+            productListBox.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            productScrollPane = new JScrollPane(productListBox);
+
+            detailsButton = new JButton("详情...");
+            clearButton = new JButton("清除");
+            exitButton = new JButton("退出");
+            shoppingButton = new JButton("查看购物车");
+
+            bottomPanel = new JPanel();
+
+            this.setLayout(new BorderLayout());
+
+            topPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+            topPanel.add(selectionLabel);
+            topPanel.add(categoryComboBox);
+
+            this.add(BorderLayout.NORTH, topPanel);
+            this.add(BorderLayout.CENTER, productScrollPane);
+
+            bottomPanel.setLayout(new FlowLayout());
+            bottomPanel.add(shoppingButton);
+            bottomPanel.add(detailsButton);
+            bottomPanel.add(clearButton);
+            bottomPanel.add(exitButton);
+
+            this.add(BorderLayout.SOUTH, bottomPanel);
+
+            detailsButton.addActionListener(new DetailsActionListener());
+            clearButton.addActionListener(new ClearActionListener());
+            exitButton.addActionListener(new ExitActionListener());
+            shoppingButton.addActionListener(new ShoppingActionListener());
+            categoryComboBox.addItemListener(new CategoryComboBoxItemListener());
+            productListBox.addListSelectionListener(new ProductListSelectionListener());
+
+            detailsButton.setEnabled(false);
+            clearButton.setEnabled(false);
+            shoppingButton.setEnabled(false);
+
+        } catch (IOException exc) {
+            JOptionPane.showMessageDialog(this, "数据访问异常：" + exc, "数据访问异常", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    /**
+     * 根据选定的类别填充商品列表
+     */
+    protected void populateListBox() {
+        try {
+            String category = (String) categoryComboBox.getSelectedItem();
+            if (!category.startsWith("---")) {
+                productArrayList = myDataClient.getProducts(category);
+            } else {
+                productArrayList = new ArrayList<>();
+            }
+
+            Product[] theData = productArrayList.toArray(new Product[productArrayList.size()]);
+
+
+            productListBox.setListData(theData);
+            productListBox.updateUI();
+
+            if (productArrayList.size() > 0) {
+                clearButton.setEnabled(true);
+            } else {
+                clearButton.setEnabled(false);
+            }
+        } catch (IOException exc) {
+            JOptionPane.showMessageDialog(this, "数据访问异常：" + exc, "数据访问异常", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+    }
+
+    /**
+     * 点击详情按钮的事件监听器
+     */
+    class DetailsActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            int index = productListBox.getSelectedIndex();
+            Product product = productArrayList.get(index);
+            ProductDetailsDialog myDetailsDialog = new ProductDetailsDialog(parentFrame, product, shoppingButton);
+            myDetailsDialog.setVisible(true);
+        }
+    }
+
+    /**
+     * 点击查看购物车按钮的事件监听器
+     */
+    class ShoppingActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            ShoppingCartDialog myShoppingDialog = new ShoppingCartDialog(parentFrame, shoppingButton);
+            myShoppingDialog.setVisible(true);
+        }
+    }
+
+    /**
+     * 点击退出按钮的事件监听器
+     */
+    class ExitActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            parentFrame.exit();
+        }
+    }
+
+    /**
+     * 点击清除按钮的事件监听器
+     */
+    class ClearActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Object[] noData = new Object[1];
+            productListBox.setListData((Product[]) noData);
+            categoryComboBox.setSelectedIndex(0);
+        }
+    }
+
+    /**
+     * 类别下拉框的选项改变事件监听器
+     */
+    class CategoryComboBoxItemListener implements ItemListener {
+        public void itemStateChanged(ItemEvent event) {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                populateListBox();
+            }
+        }
+    }
+
+    /**
+     * 商品列表的选择事件监听器
+     */
+    class ProductListSelectionListener implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent event) {
+            if (productListBox.isSelectionEmpty()) {
+                detailsButton.setEnabled(false);
+            } else {
+                detailsButton.setEnabled(true);
+            }
+        }
+    }
 }
